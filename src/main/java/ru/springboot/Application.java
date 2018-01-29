92d30fae4c866403631ef904de800443c22e7a6c
Avsievich <Avsievichv@yandex.ru>
2018-01-29 15:20:32 +0400

    1

--- src/main/java/ru/springboot/Application.java
+++ src/main/java/ru/springboot/Application.java
@@ -2,10 +2,11 @@ package ru.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws Exception {
@@ -14,6 +15,6 @@ public class Application {
 
     @RequestMapping("/hello")
     String home() {
-        return "Hello World";
+        return "hello-world";
     }
     }
